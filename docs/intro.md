a8a71b59983b8aa2be440cc998eb92729d499b38
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2026-06-02 23:23:00 +0300

    Install Docusaurus and initial documentation setup

---
+++ docs/intro.md
@@ -0,0 +1,13 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+
+Welcome to the fuck-ecoflow documentation!
+
+This documentation provides information about the fuck-ecoflow project.
+
+## Getting Started
+
+To get started with this project, please refer to the documentation sections below.
---
+++ docusaurus.config.js
@@ -0,0 +1,110 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Fuck Ecoflow',
+  tagline: 'Documentation for fuck-ecoflow',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://atherdon.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/fuck-ecoflow/',
+
+  // GitHub pages deployment config.
+  organizationName: 'atherdon',
+  projectName: 'fuck-ecoflow',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is in Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Fuck Ecoflow',
+        logo: {
+          alt: 'Fuck Ecoflow Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/atherdon/fuck-ecoflow',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'GitHub Issues',
+                href: 'https://github.com/atherdon/fuck-ecoflow/issues',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} atherdon. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
